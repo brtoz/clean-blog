6514d9294d56ed999e94635b83ae1d985bda02f7
Berat <beratozfidan@hotmail.com>
2024-04-16 17:54:13 +0300

    updtae

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  
+  const photo = {
+    id: 1,
+    title: 'Blog title',
+    description: 'Blog Description'
+  }
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
