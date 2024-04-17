8d22d893d78c3fbf4ec3a4fa5dc3d46ef8529ed5
Berat <beratozfidan@hotmail.com>
2024-04-17 14:44:59 +0300

    Files Updated

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  
-  const photo = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog Description'
-  }
-  res.send(photo);
+//TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+// MIDDLEWARE
+app.use(express.static("public"));
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
+
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı.`);
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
