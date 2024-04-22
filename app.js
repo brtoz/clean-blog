17ad0ed1d1f8554dba89daeaefefef3f42922886
Berat <beratozfidan@hotmail.com>
2024-04-22 15:02:22 +0300

    Files Update

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
 const express = require("express");
-const ejs = require("ejs");
-
+const mongoose = require('mongoose');
+const ejs =require('ejs');
+const path = require("path");
+const post = require('./models/newPost');
 const app = express();
 
-//TEMPLATE ENGINE
+// connect  DB
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
+//TEMPLATE ENGİNE
 app.set("view engine", "ejs");
 
-// MIDDLEWARE
-app.use(express.static("public"));
-
-//ROUTES
-app.get("/", (req, res) => {
-  res.render("index");
+// MIDDLEWARES
+app.use(express.static('public')); // Static dosyaları koyacağımız klasörü seçtik
+app.use(express.urlencoded({ extended: true })); // Body parser okuyoruz
+app.use(express.json()); // Body parser dönüştürüyoruz
+// ROUTES
+app.get("/", async (req, res) => {
+  const addPost = await post.find({})
+  // dosyayo içeri çektik
+  // express static middleware kullandık
+  //res.sendFile(path.resolve(__dirname, "tmp/index.html"));
+  res.render('index',{
+    addPost
+  })
+ 
 });
-
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render('about')
 });
-
-app.get("/add_post", (req, res) => {
-  res.render("add_post");
-});
-
 app.get("/post", (req, res) => {
-  res.render("post");
+  res.render('post')
 });
+app.get("/contact", (req, res) => {
+  res.render('contact')
+});
+app.get("/addNewPost", (req, res) => {
+  res.render('addNewPost')
+});
+app.post('/photos', async (req, res) => { // async - await yapısı kullanacğız.
+  await post.create(req.body)// body bilgisini Photo modeli sayesinde veritabanında dökümana dönüştürüyoruz.
+  res.redirect('/')
+});
+
+app.get('/', async (req, res) => {
+  const addPost = await posts.find({})
+  res.render('index', {
+    addPost
+  })});
+
+
 
 const port = 3000;
-
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+  console.log(`sunucu ${port} portunda başlatıldı`);
 });
